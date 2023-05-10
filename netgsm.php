bb0a62b8b6bbe137b673ccd3a153829f66d5fdf9
Erkan Kavas <118114166+erkankavas@users.noreply.github.com>
2023-05-10 12:30:13 +0300

    Add files via upload
    netgsm first v :) php

---
+++ netgsm.php
@@ -0,0 +1,22 @@
+<?php
+
+//netgsm turkish voip sms operator - php api by ERKANKAVAS 2023 :P
+//have a question please contact @erkankavas in twitter :P
+
+$username = "850xxxxxxxxxx"; // tel number
+$password = urlencode("xxxxxxxxxx"); // pass api key
+
+$tel = "5xxxxxxxxx"; //who you wanna send sms :)
+$mes = "merhaba!"; // message you wanna send.
+      
+$url= "https://api.netgsm.com.tr/sms/send/get/?usercode=".$username."&password=".$password."&gsmno=".$tel."&message=".$mes."&msgheader=".$username;
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$http_response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+//you can track response or http code :) for check :P
+
+?>
